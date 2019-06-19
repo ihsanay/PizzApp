62ad5c4a26b2b30fd3ee160d91d1ab4f61d6856e
ihsanay <ihsanay1990@gmail.com>
2019-06-19 22:29:40 +0300

    Nhibernate, IQueryRepository’nin NHibernate için implementasyonu yapıldı

---
+++ PizzApp/PizzApp.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,35 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzApp.Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+
+        private static ISessionFactory _sessionFactory;
+        //factory design pattern'den besleniyor. Oracle için configuration gönderildiğinde, oracle için session oluşturacak. Sql server için gönderildiğinde onun için...
+
+        //virtual - lazy loading veya navigation property bir nesnede, o nesnenin ilişkili nesnelerini getirmek gibi durumlar için.
+        public virtual ISessionFactory SessionFactory
+        {
+            get { return _sessionFactory ?? (_sessionFactory = InitializeFactory()); }
+        }
+
+        //NHibernateHelper'ı kim kullanacaksa, ona şunu demek lazım: bir session factory implementasyonu gerçekleştir. Ben onu kullanayım. Burada da bir nevi dependency injection gibi düşünülebilir. Dolayısıyla, bunu kullanacak kişi, ezeceğine göre, NHibernateHelper'ı implement ettiğinde hepsinin ayrı ayrı implement etmesi için abstract bir metod yazmak lazım. (Sadece implemente eden kullansın diye protected)
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ PizzApp/PizzApp.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,44 @@
+﻿using PizzApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzApp.Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+
+        private IQueryable<T> _entities; //bundan sonra Table'a return.
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+        //public IQueryable<T> Table => throw new NotImplementedException();
+        public IQueryable<T> Table
+        {
+            get
+            {
+                return this.Entities;
+            }
+
+        }
+
+        public virtual IQueryable<T> Entities //_entities dönecek
+        {
+            get
+            {
+                if (_entities == null)
+                {
+                    //tüm nesneye abone
+                    _entities = _nHibernateHelper.OpenSession().Query<T>();
+                }
+                return _entities;
+            }
+
+
+        }
+    }
+}
