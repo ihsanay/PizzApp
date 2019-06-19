62aab1bf166ec1f38cd9b69f62ed3397301b948e
ihsanay <ihsanay1990@gmail.com>
2019-06-19 20:43:53 +0300

    Entity Framework implementasyonu yapıldı. EF metod içerikleri dolduruldu.

---
+++ PizzApp/PizzApp.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using PizzApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace PizzApp.Core.DataAccess.EntityFramework
+{
+    class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity> 
+        where TEntity : class, IEntity, new() 
+        where TContext : DbContext, new()
+    {
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            //using - context disposable olduğu için
+            using (var context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+    }
+}
--- PizzApp/PizzApp.Core/DataAccess/IQueryableRepository.cs
+++ PizzApp/PizzApp.Core/DataAccess/IQueryableRepository.cs
@@ -1,9 +1,5 @@
 ﻿using PizzApp.Core.Entities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PizzApp.Core.DataAccess
 {
