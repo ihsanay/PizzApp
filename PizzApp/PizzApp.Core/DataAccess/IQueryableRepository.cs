4f35dd62d59349a58f39d81acf994405b8b19cd7
ihsanay <ihsanay1990@gmail.com>
2019-06-19 18:47:44 +0300

    Core / DataAccess için Repository'ler oluşturuldu.

---
+++ PizzApp/PizzApp.Core/DataAccess/IQueryableRepository.cs
@@ -0,0 +1,18 @@
+﻿using PizzApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzApp.Core.DataAccess
+{
+    /// <summary>
+    /// IQueryable operasyonların (özellikle business'da) çalıştırılabilmesi için (context kapanmadan çalıştırılabilmesi için) 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        IQueryable<T> Table { get; }
+    }
+}
