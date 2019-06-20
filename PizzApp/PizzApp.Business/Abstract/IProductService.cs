04a931e62928a0dea5e6b12bc412608f55484e73
ihsanay <ihsanay1990@gmail.com>
2019-06-20 20:03:20 +0300

    İş katmanı implementasyonu, temel db işlemleri.

---
+++ PizzApp/PizzApp.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using PizzApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzApp.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ PizzApp/PizzApp.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using PizzApp.Business.Abstract;
+using PizzApp.DataAccess.Abstract;
+using PizzApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzApp.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.Id == id);
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _productDal.GetList();
+        }
+    }
+}
