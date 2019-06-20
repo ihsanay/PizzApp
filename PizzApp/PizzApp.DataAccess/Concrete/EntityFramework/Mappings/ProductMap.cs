34127e83d4f438d5d143357c01280e06b92342f9
ihsanay <ihsanay1990@gmail.com>
2019-06-20 12:00:03 +0300

    Product Mapping Implementasyonu yapıldı.

---
+++ PizzApp/PizzApp.DataAccess/Concrete/EntityFramework/Mappings/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using PizzApp.Entities.Concrete;
+using System.Data.Entity.ModelConfiguration;
+
+namespace PizzApp.DataAccess.Concrete.EntityFramework.Mappings
+{
+    class ProductMap : EntityTypeConfiguration<Product>
+    {
+        public ProductMap()
+        {
+            ToTable(@"Products", @"dbo");
+            HasKey(x => x.Id);
+            Property(x => x.Id).HasColumnName("Id");
+            Property(x => x.ProductTypeId).HasColumnName("ProductTypeId");
+            Property(x => x.Name).HasColumnName("Name");
+            Property(x => x.Price).HasColumnName("Price");
+            Property(x => x.Desc).HasColumnName("Desc");
+            Property(x => x.DiscountPrice).HasColumnName("DiscountPrice");
+        }
+    }
+}
